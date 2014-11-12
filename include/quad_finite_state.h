28133cf4e1d6c3a6918fe445ec7d3730de47adf3
Rowland O'Flaherty <rowoflo@gmail.com>
2014-11-12 12:43:05 -0500

    Initial add of quad_finite_state, quad, quad_manager files.

---
+++ include/quad_finite_state.h
@@ -0,0 +1,76 @@
+/*
+@brief Enums and class defining the quad's finite states
+
+@author Rowland O'Flaherty
+@date 11/12/2014
+*/
+
+#ifndef QUAD_FINITE_STATES_H
+#define QUAD_FINITE_STATES_H
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+#include <cassert>
+
+//------------------------------------------------------------------------------
+// Constants and Enums
+//------------------------------------------------------------------------------
+
+// Finite States
+const int FINITE_STATE_DIM = 3;
+const int FINITE_STATE_DIM_SIZE [FINITE_STATE_DIM] = {2, 4, 2};
+
+// DIM 0
+const int ARM_STATE = 0;
+enum ArmState
+{
+    DISARMED=0,
+    ARMED
+};
+
+// DIM 1
+const int FLIGHT_STATE = 1;
+enum FlightState
+{
+    GROUNDED=0,
+    HOLDING,
+    AUTONOMOUS,
+    TELEOP
+};
+
+// DIM 2
+const int SETPOINT_STATE = 2;
+enum SetpointState
+{
+    AT_SETPOINT = 0,
+    GOINT_TO_SETPOINT
+};
+
+// Transitions
+const int NUM_OF_COMMANDS = 4;
+enum Commands
+{
+    TOGGLE_ARM=0,
+    TAKEOFF,
+    FLY_TO,
+    LAND
+};
+
+
+//==============================================================================
+// QuadFiniteState Class
+//==============================================================================
+class QuadFiniteState
+{
+private:
+    int m_finite_state[FINITE_STATE_DIM];
+
+public:
+    int& operator[] (const int n_index) {
+        assert(n_index >= 0 && n_index < FINITE_STATE_DIM);
+        return m_finite_state[n_index];
+    }
+};
+
+#endif
---
+++ test/quad_test.cpp
@@ -0,0 +1,46 @@
+/**
+ * @file quad_test.cpp
+ * @author Rowland O'Flaherty
+ * @date 11/12/2014
+ */
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+#include "quad_manager.h"
+#include "quad.h"
+#include "quad_finite_state.h"
+
+#include <iostream>
+
+//------------------------------------------------------------------------------
+// Namespaces
+//------------------------------------------------------------------------------
+using namespace std;
+
+//------------------------------------------------------------------------------
+// Main Function
+//------------------------------------------------------------------------------
+int main(int argc, char *argv[])
+{
+    cout << "" << endl;
+    cout << "*===============================*" << endl;
+    cout << "|  TEST PROGRAM FOR quad CLASS  |" << endl;
+    cout << "*===============================*" << endl;
+    cout << endl;
+
+    // Quad q;
+
+    // cout << q.finite_state[0] << endl;
+    // q.finite_state[0] = 1;
+    // cout << q.finite_state[0] << endl;
+    // cout << q.id() << endl;
+    // q.id(4);
+    // cout << q.id() << endl;
+
+    // QuadManager qm(3);
+    // for (int i=0; i<3; ++i) {
+    //     cout << qm.quad
+    // }
+}
+
